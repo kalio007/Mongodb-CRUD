9eabf871088bbcd1c2868895735dca2b10423e36
kalio007 <kalioprince1@gmail.com>
2023-08-30 23:42:53 +0100

    first commit

---
+++ myApp.js
@@ -0,0 +1,81 @@
+require('dotenv').config();
+let uri = 'mongodb+srv://user1:'+ process.env.PW + '@lighthouse.8sdhpcv.mongodb.net/?retryWrites=true&w=majority';
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const peopleSchema = new Schema({
+  name: {String, required:true},
+  age: Number,
+  body: String,
+  favfood: [String],
+});
+let Person = mongoose.model('Person', peopleSchema);
+
+// let Person;
+
+const createAndSavePerson = (done) => {
+  done(null /*, data*/);
+};
+
+const createManyPeople = (arrayOfPeople, done) => {
+  done(null /*, data*/);
+};
+
+const findPeopleByName = (personName, done) => {
+  done(null /*, data*/);
+};
+
+const findOneByFood = (food, done) => {
+  done(null /*, data*/);
+};
+
+const findPersonById = (personId, done) => {
+  done(null /*, data*/);
+};
+
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = "hamburger";
+
+  done(null /*, data*/);
+};
+
+const findAndUpdate = (personName, done) => {
+  const ageToSet = 20;
+
+  done(null /*, data*/);
+};
+
+const removeById = (personId, done) => {
+  done(null /*, data*/);
+};
+
+const removeManyPeople = (done) => {
+  const nameToRemove = "Mary";
+
+  done(null /*, data*/);
+};
+
+const queryChain = (done) => {
+  const foodToSearch = "burrito";
+
+  done(null /*, data*/);
+};
+
+/** **Well Done !!**
+/* You completed these challenges, let's go celebrate !
+ */
+
+//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+
+exports.PersonModel = Person;
+exports.createAndSavePerson = createAndSavePerson;
+exports.findPeopleByName = findPeopleByName;
+exports.findOneByFood = findOneByFood;
+exports.findPersonById = findPersonById;
+exports.findEditThenSave = findEditThenSave;
+exports.findAndUpdate = findAndUpdate;
+exports.createManyPeople = createManyPeople;
+exports.removeById = removeById;
+exports.removeManyPeople = removeManyPeople;
+exports.queryChain = queryChain;
